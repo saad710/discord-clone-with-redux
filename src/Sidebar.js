6fc192b90f26bf77e1f5b566225e4ba4cd4c57de
saad710 <ifrathasnat2@gmail.com>
2021-02-02 17:09:42 +0600

    discord-clone

---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Sidebar.css";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* <h2>I am the sidebar</h2> */}
+            <div className="sidebar_top">
+                <h3>Anonymous Programmer</h3>
+                <ExpandMoreIcon/>
+            </div>
+            <div className="sidebar_channels">
+                <div className="sidebar_channels_header">
+                    <div className="sidebar_header">
+                        <ExpandMoreIcon/>
+                        <h4>Text Channels</h4>
+                    </div>
+                    <AddIcon className="sidebar_addChannel"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
