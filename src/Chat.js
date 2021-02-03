b9d12e5b387e6f50f1accef192259f9397be5b17
saad710 <ifrathasnat2@gmail.com>
2021-02-03 14:23:02 +0600

    chat-header-done

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Chat.css';
+import ChatHeader from './ChatHeader';
 
 function Chat() {
     return (
         <div className="chat">
-            <h2>I am the chat screen</h2>
+            {/* <h2>I am the chat screen</h2> */}
+            <ChatHeader/>
         </div>
     )
 }
