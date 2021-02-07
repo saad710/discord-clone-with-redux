be5d3b82d7eb5d7c0c70fc226806d63182c09b6e
saad710 <ifrathasnat2@gmail.com>
2021-02-07 18:46:25 +0600

    login-logout-done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,9 +10,13 @@ import { Avatar } from '@material-ui/core';
 import MicIcon from '@material-ui/icons/Mic';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import SettingsIcon from '@material-ui/icons/Settings';
+import {selectUser} from "./features/counter/userSlice"
+import { useSelector } from 'react-redux';
+import {auth} from "./firebase";
 
 
 function Sidebar() {
+    const user = useSelector(selectUser);
     return (
         <div className="sidebar">
             {/* <h2>I am the sidebar</h2> */}
@@ -50,10 +54,10 @@ function Sidebar() {
                     </div>
             </div>
             <div className="sidebar_profile">
-                <Avatar/>
+                <Avatar onClick={() => auth.signOut()} src= {user.photo}/>
                 <div className="sidebar_profileInfo">
-                    <h3>Ifrat Hasnat</h3>
-                    <p>#thisIsMyID</p>
+                    <h3>{user.displayName}</h3>
+                    <p>{user.uid.substring(0,5)}</p>
                 </div>
                 <div className="sidebar_profileIcons">
                     <MicIcon/>
